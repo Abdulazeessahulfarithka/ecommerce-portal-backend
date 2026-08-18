fede5c08448051c8f40a9f072f0905e7945a51f6
“Abdul <kaazees31@gmail.com>
2026-08-18 10:30:22 +0530

    update in multer

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -24,7 +24,7 @@ export  const registerUser = async (req,res)=>{
        const hashedPassword = await bcrypt.hash(password,10)
 
        const user = await User.create({
-        name,email,phone,password:hashedPassword,
+        name,email,password:hashedPassword,
        })
        res.status(201).json({
         success:true,
--- Model/User.js
+++ Model/User.js
@@ -15,6 +15,10 @@ const UserSchema = new mongoose.Schema(
         type:String,
         required:true,
      },
+      phone:{
+        type:String,
+        required:true,
+     },
      isAdmin: { type: Boolean, default: false },
      googleId: { type: String, unique: true, sparse: true },
        phone: { type: String },
