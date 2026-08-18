54d03595a9a83acf615899ed577439fefa05ea89
“Abdul <kaazees31@gmail.com>
2026-08-18 10:52:48 +0530

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors({
     origin:[
         "http://localhost:5173",
     ],
-    method:["GET","POST","PUT","DELETE"],
+    methods:["GET","POST","PUT","DELETE"],
     credentials:true,
     allowedHeaders:["Content-Type","Authorization"]
 }))
